f36a3eafba495636b519411d6dbfcfcb29be8a3b
Jen <jenniferlynnebarry@gmail.com>
2022-07-12 16:30:52 -0400

    testing for Circle CI

--- tests/server.spec.js
+++ tests/server.spec.js
@@ -37,4 +37,15 @@ describe('server/app.js', function() {
       done();
     });
   });
+
+  it('page asks "Do your tests pass?"', (done) => {
+    chai.request(server)
+      .get('/')
+      .end((err, res) => {
+        expect(err).not.exist;
+        expect(JSON.stringify(res.text)).to.contain('Do your tests pass?');
+        done();
+      });
+    });
+
 })
